09d7fb22278cb67223b62f8ac9663518f2ea13d2
ironAtom <tqinyu@gmail.com>
2015-04-13 11:13:02 -0400

    Initial Commit

---
+++ Sonorus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sonorus
+//
+//  Created by Qinyu Tong on 4/13/15.
+//  Copyright (c) 2015 DS-Team15. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
